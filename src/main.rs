bd37622e6e6a57d9d4904bd147f25ed281970c84
Nikolay Neupokoev <neupokoev.n@gmail.com>
2024-01-13 16:26:05 -0800

    Meta states

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::convert::TryInto;
 use std::hash::Hash;
+use std::io::{Error, Result};
 use rand::Rng;
 use plotters::{prelude::*, data};
 use ndarray::{array, Array2, Axis};
@@ -92,22 +93,110 @@ fn plot_graph(
     )).unwrap();
 }
 
+const ACTIONS: [Motion; 4] = [
+  Motion::North(1),
+  Motion::South(1),
+  Motion::East(1),
+  Motion::West(1)
+];
+  
+const STATES: [char; 5] = [
+  'S',
+  'F',
+  'H',
+  'G',
+  'W',
+];
+
+struct StateEncoder {
+  states: Vec<char>,
+  encoding_map: HashMap<char, i8>,
+}
+
+impl StateEncoder {
+    pub fn new(&mut self) {
+      self.states = STATES.to_vec();
+    }
+
+    pub fn encode(&self, state: char) -> i8 {
+      self.encoding_map[&state]
+    }
+}
+
+type GridPos = [usize; 2];
+type MetaState = [char; 5];
+type PossibleActions = [f32; 4];
+type RewardMap = HashMap<char, i32>;
+
+fn near_location(loc: GridPos, mov: [i32; 2]) -> Result<GridPos> {
+  let x = mov[0] + loc[0] as i32;
+  if x < 0 {
+    return Err(Error::other("x < 0"));
+  }
+  let y = mov[1] + loc[1] as i32;
+  if y < 0 {
+    return Err(Error::other("y < 0"));
+  }
+  let new_loc = [x as usize, y as usize];
+  Ok(new_loc)
+}
+
+fn create_meta_state(
+  loc: GridPos, 
+  gw: &GridWorld<char>, 
+  reward_map: &RewardMap, 
+  default_value: char) 
+-> MetaState {
+  let mut meta_state: MetaState = [default_value; 5];
+  let moves: [[i32; 2]; 5] = [
+    [0, 0],
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ];
+  for (i, mov) in moves.into_iter().enumerate() {
+    if let Ok(new_loc) = near_location(loc, mov) {
+
+      let state = gw.get(new_loc).unwrap();
+      let r = reward_map.get(state).unwrap().to_owned();
+      if r != 0 {
+        meta_state[i] = state.to_owned();
+      }
+    } else {
+      // TODO: need to avoid this situation (don't go into walls, for example)
+      let state = gw.get(loc).unwrap();
+      let r = reward_map.get(state).unwrap().to_owned();
+      if r != 0 {
+        meta_state[i] = state.to_owned();
+      }
+    }
+  }
+  return meta_state;
+}
+
+// fn create_meta_state<T>(loc: [usize; 2], gw: &GridWorld<T>, reward_map: &HashMap<T, i32>, default_value: T) -> [T; 5] 
+// where T: Hash + Copy, T: std::cmp::Eq {
+//   let mut meta_state: [T; 5] = [default_value; 5];
+//   let moves: [[i32; 2]; 5] = [
+//     [0, 0],
+//     [-1, 0],
+//     [1, 0],
+//     [0, -1],
+//     [0, 1],
+//   ];
+//   for (i, mov) in moves.into_iter().enumerate() {
+//     let new_loc = [(mov[0] + loc[0] as i32) as usize, (mov[1] + loc[1] as i32) as usize];
+//     let state = gw.get(new_loc).unwrap();
+//     let r = reward_map.get(state).unwrap().to_owned();
+//     if r != 0 {
+//       meta_state[i] = state.to_owned();
+//     }
+//   }
+//   return meta_state;
+// }
+
 fn simulate() {
-  // actions
-  let actions = vec! [
-    Motion::North(1),
-    Motion::South(1),
-    Motion::East(1),
-    Motion::West(1)
-  ];
-  // let mut states: Vec<char> = Vec::new();
-  let states = vec! [
-    'S',
-    'F',
-    'H',
-    'G',
-    'W',
-  ];
   let mut world: Array2<char> = array![
     ['W', 'W', 'W', 'W', 'W', 'W'],
     ['W', 'S', 'F', 'F', 'F', 'W'],
@@ -119,7 +208,7 @@ fn simulate() {
   world.swap_axes(0, 1);
   let gw = GridWorld::<char>::new(world);
 
-  let reward_map = HashMap::from([
+  let reward_map: RewardMap = HashMap::from([
     ('S', 0),
     ('F', 0),
     ('H', -10),
@@ -127,7 +216,7 @@ fn simulate() {
     ('W', -1),
   ]);
 
-  let mut transition_prob = Array2::<f32>::zeros((states.len(), actions.len()));
+  let mut transition_prob: HashMap<MetaState, PossibleActions> = HashMap::new();
   // # actions = rows
   // # states = columns
   //        North South East West
@@ -135,21 +224,15 @@ fn simulate() {
   // Free  [                     ]
   // Hole  [                     ]
   // Goal  [                     ]
-  for mut row in transition_prob.axis_iter_mut(Axis(0)) {
-    let p = 1.0 / (row.len() as f32);
-    for e in row.iter_mut() {
-      *e = p;
-    }
-  }
-  // for row in transition_prob.rows() {
-  //   let p = 1.0 / (actions.len() as f32);
-  //   for e in row {
-  //     e = p;
+  
+  // for mut row in transition_prob.axis_iter_mut(Axis(0)) {
+  //   let p = 1.0 / (row.len() as f32);
+  //   for e in row.iter_mut() {
+  //     *e = p;
   //   }
   // }
-
+  
   let mut rng = rand::thread_rng();
-  // let final_state = 'G';
   let max_iter = 50;
   let mut data_points_bad = Vec::new();
   let mut data_points_good = Vec::new();
@@ -157,27 +240,39 @@ fn simulate() {
 
   for i in 0..max_iter {
     println!("--- {} ---", i);
-    let mut loc = [1, 1];
+    let mut loc: GridPos = [1, 1];
     let mut step = 0;
     let mut reward = 0;
     'steps: loop {
       step += 1;
 
-      let p: f32 = rng.gen();
       // let random_action = actions[p];
-      let current_state = gw.get(loc).unwrap().clone();
-      let state_id = states.iter().position(|&c| c == current_state).unwrap();
-      let mut cumulative_prob = 0.0;
+      // let current_state = gw.get(loc).unwrap().to_owned();
+      let meta_state = create_meta_state(loc, &gw, &reward_map, '_');
+      
+      // find id for the state
+      // let state_id = states.iter().position(|&c| c == current_state).unwrap();
+      
       let mut next_action_id = None;
-
-      for (i, &weight) in transition_prob.row(state_id).iter().enumerate() {
-        cumulative_prob += weight;
-        if p <= cumulative_prob {
-          next_action_id = Some(i);
-          break;
+      if let Some(state_probs) = transition_prob.get(&meta_state) {
+        let p: f32 = rng.gen();
+        let mut cumulative_prob = 0.0;
+        for (i, &weight) in state_probs.iter().enumerate() {
+          cumulative_prob += weight;
+          if p <= cumulative_prob {
+            next_action_id = Some(i);
+            break;
+          }
         }
+      } else {
+        let p = 1.0 / (ACTIONS.len() as f32);
+        let mut even_spread: [f32; 4] = [p; 4];
+        // for e in even_spread.iter_mut() {
+        //   *e = p;
+        // }
+        transition_prob.insert(meta_state, even_spread);
+        next_action_id = Some(rng.gen_range(0..ACTIONS.len()));
       }
-
       match next_action_id {
         Some(action) => {
           println!("Randomly selected action: {}", action);
@@ -186,25 +281,33 @@ fn simulate() {
           println!("No action selected. Check your weights.");
         }
       }
-      let random_action = actions[next_action_id.unwrap()];
+      let random_action = ACTIONS[next_action_id.unwrap()];
 
-      loc = gw.perform_motion(loc, random_action);
-      let state: &char = gw.get(loc).unwrap();
-      
-      let step_reward = reward_map.get(state).unwrap().clone();
+      let new_loc = gw.perform_motion(loc, random_action);
+      let state: &char = gw.get(new_loc).unwrap();
+      if state == &'W' {
+        println!("This move is not allowed (-> W), keeping last position");
+      } else {
+        loc = new_loc;
+      }
+      let step_reward = reward_map.get(state).unwrap().to_owned();
       if step_reward != 0 {
-        let triggered_prob = transition_prob.get((state_id, next_action_id.unwrap())).unwrap();
-        let delta = (step_reward as f32) * triggered_prob / 2.0;
-        let delta_rem = delta / ((actions.len() - 1) as f32);
-        let mut row = transition_prob.row_mut(state_id);
-        for (i, e) in row.indexed_iter_mut() {
-          if i == next_action_id.unwrap() {
-            *e += delta;
-          } else {
-            *e -= delta_rem;
+        if let Some(state_probs) = transition_prob.get_mut(&meta_state) {
+          let triggered_prob = state_probs[next_action_id.unwrap()];  
+          let delta = (step_reward as f32) * triggered_prob / 2.0;
+          let delta_rem = delta / ((ACTIONS.len() - 1) as f32);
+          for (i, e) in state_probs.iter_mut().enumerate() {
+            if i == next_action_id.unwrap() {
+              *e += delta;
+            } else {
+              *e -= delta_rem;
+            }
           }
+        } else {
+          println!("meta state disappeared :O");
         }
-
+      } else {
+        println!("no reward = no prob updates");
       }
       reward += step_reward;
       
