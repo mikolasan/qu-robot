95a366a20f1b6f282c5a3bff952262b739d1646b
Nikolay Neupokoev <neupokoev.n@gmail.com>
2024-02-03 22:25:03 -0800

    Tried weak references, switched to RefCell

--- src/neuron.rs
+++ src/neuron.rs
@@ -1,78 +1,83 @@
-use std::rc::Rc;
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
 use std::collections::HashMap;
 use uuid::Uuid;
 
 use crate::scheduler::Scheduler;
 
-struct Axon<'a> {
-  neuron: &'a Neuron<'a>,
-  connections: HashMap<[u8; 16], Vec<Rc<Dendrite<'a>>>>,
+struct Axon {
+  // neuron: Weak<Neuron>,
+  connections: HashMap<[u8; 16], Vec<Weak<Dendrite>>>,
 }
 
-impl<'a> Axon<'a> {
-  pub fn new(neuron: &Neuron) -> Self {
+impl Axon {
+  pub fn new() -> Self {
     Axon {
-      neuron,
       connections: HashMap::new(),
     }
   }
 
-  fn add_dendrite(&mut self, neuron: Rc<Neuron>, dendrite: Rc<Dendrite<'a>>) {
-    let neuron_id = neuron.id.as_bytes();
+  fn add_dendrite(&mut self, neuron: Weak<Neuron>, dendrite: Weak<Dendrite>) {
+    let neuron_id = neuron.upgrade().unwrap()
+      .id.as_bytes();
     if !self.connections.contains_key(neuron_id) {
-      self.connections.insert(*neuron_id, vec![dendrite]);
-    } else {
-      let dendrites = self.connections.get_mut(neuron_id).unwrap();
-      dendrites.push(dendrite);
+      self.connections.insert(*neuron_id, Vec::new());
     }
+    let dendrites = self.connections.get_mut(neuron_id).unwrap();
+    dendrites.push(dendrite);
   }
 
   fn activate(&mut self, time: u64, output: f64) {
     for connections in self.connections.values_mut() {
       for dendrite in connections {
-        dendrite.activate(time, output);
+        dendrite.upgrade().unwrap()
+          .activate(time, output);
       }
     }
 
   }
 }
 
-struct Dendrite<'a> {
-  neuron: Rc<Neuron<'a>>,
+struct Dendrite {
+  neuron: Weak<Neuron>,
   strength: f64,
 }
 
-impl<'a> Dendrite<'a> {
-  pub fn new(neuron: Rc<Neuron<'a>>, strength: f64) -> Self {
-    Dendrite { neuron, strength }
+impl Dendrite {
+  pub fn new(neuron: Weak<Neuron>, strength: f64) -> Self {
+    Dendrite {
+      neuron,
+      strength,
+    }
   }
 
   pub fn activate(&mut self, time: u64, input: f64) {
     if self.strength > 0.5 {
-      println!("{{time {}}} -> ({}) {{{}}}", time, self.neuron.name, input);
-      self.neuron.record_input(time, input);
+      println!("{{time {}}} -> ({}) {{{}}}", time, self.neuron.upgrade().unwrap().name, input);
+      self.neuron.upgrade().unwrap()
+        .record_input(time, input);
       self.strength += 0.1;
     }
   }
 }
 
-pub struct Neuron<'a> {
-  dendrites: Vec<Box<Dendrite<'a>>>,
-  axon: Option<Axon<'a>>,
+pub struct Neuron {
+  dendrites: Vec<Rc<Dendrite>>,
+  axon: Rc<Axon>,
   plasticity_on: bool,
   inputs: HashMap<u64, Vec<f64>>,
   name: String,
   pub id: Uuid,
   transform_fn: fn(Vec<f64>) -> f64,
-  scheduler: Rc<Scheduler<'a>>,
+  scheduler: RefCell<Scheduler>,
   activation_delay: u64,
 }
 
-impl<'a> Neuron<'a> {
-  pub fn new(name: String, scheduler: Rc<Scheduler>, activation_delay: u64) -> Self {
-    let mut neuron = Neuron {
+impl Neuron {
+  pub fn new(name: String, scheduler: RefCell<Scheduler>, activation_delay: u64) -> Self {
+    Neuron {
       dendrites: Vec::new(),
-      axon: None,
+      axon: Rc::new(Axon::new()),
       plasticity_on: false,
       inputs: HashMap::new(),
       name,
@@ -80,30 +85,30 @@ impl<'a> Neuron<'a> {
       transform_fn: sum,
       scheduler,
       activation_delay,
-    };
-    let axon = Some(Axon::new(&neuron));
-    neuron.axon = axon;
-    neuron
+    }
   }
 
-  fn add_connection(&mut self, neuron: &mut Neuron, strength: Option<f64>) {
-    let this_neuron = self as &Neuron;
-    let dendrite = Box::new(Dendrite::new(
-      Rc::new(*this_neuron),
+  fn add_connection(mut self: Rc<Self>, neuron: &mut Rc<Neuron>, strength: Option<f64>) {
+    let dendrite = Rc::new(Dendrite::new(
+      Rc::downgrade(&self),
       strength.unwrap_or_else(|| rand::random())
     ));
     // add connections to pre-synaptic neuron
-    let mut axon = neuron.axon.unwrap();
-    axon.add_dendrite(Rc::new(*this_neuron), Rc::new(*dendrite));
+    neuron.axon.add_dendrite(
+      Rc::downgrade(&self),
+      Rc::downgrade(&dendrite)
+    );
     // add connections to post-synaptic neuron (this)
     self.dendrites.push(dendrite);
   }
 
-  fn add_input(&mut self, sensor: &mut Sensor) {
-    let this_neuron = self as &Neuron;
-    let dendrite = Box::new(Dendrite::new(Rc::new(*this_neuron), 1.0));
+  fn add_input(mut self: Rc<Self>, sensor: &mut Sensor) {
+    let dendrite = Rc::new(Dendrite::new(
+      Rc::downgrade(&self),
+      1.0
+    ));
     // pre-synaptic part
-    sensor.connections.push(Rc::new(*dendrite));
+    sensor.connections.push(Rc::downgrade(&dendrite));
     // add connections to post-synaptic neuron (this)
     self.dendrites.push(dendrite);
   }
@@ -115,24 +120,23 @@ impl<'a> Neuron<'a> {
     } else if let Some(inputs) = self.inputs.get_mut(&activation_time) {
       inputs.push(input);
     }
-    let this_neuron = self as &Neuron;
-    self.scheduler.add(activation_time, self);
+    self.scheduler.get_mut().add(activation_time, self);
   }
 
   pub fn activate(&mut self, time: u64) {
     let output = (self.transform_fn)(self.inputs[&time].clone());
     println!("{{time {}}} Neuron({}) output -> {}", time, self.name, output);
     self.inputs.remove(&time);
-    self.axon.unwrap().activate(time + 1, output);
+    self.axon.activate(time + 1, output);
   }
 }
 
 
-struct Sensor<'a> {
-  connections: Vec<Rc<Dendrite<'a>>>,
+struct Sensor {
+  connections: Vec<Weak<Dendrite>>,
 }
 
-impl<'a> Sensor<'a> {
+impl Sensor {
   // Implement Sensor methods as needed
 }
 
@@ -142,6 +146,7 @@ fn sum(values: Vec<f64>) -> f64 {
 
 #[cfg(test)]
 mod tests {
+  use std::cell::RefCell;
   use std::rc::Rc;
   use crate::neuron::{Neuron, Sensor};
   use super::Scheduler;
@@ -150,14 +155,24 @@ mod tests {
   #[test]
   fn main() {
     // Example usage
-    let mut scheduler = Rc::new(Scheduler::new());
-    let mut neuron1 = Neuron::new("Neuron1".to_string(), Rc::clone(&scheduler), 0);
-    let mut neuron2 = Neuron::new("Neuron2".to_string(), Rc::clone(&scheduler), 0);
-    let mut sensor = Sensor { connections: Vec::new() };
+    let mut scheduler = RefCell::new(Scheduler::new());
+    let mut neuron1 = Neuron::new(
+      "Neuron1".to_string(), 
+      scheduler, 
+      0
+    );
+    let mut neuron2 = Neuron::new(
+      "Neuron2".to_string(), 
+      scheduler, 
+      0
+    );
+    let mut sensor = Sensor { 
+      connections: Vec::new() 
+    };
 
-    neuron1.add_connection(&mut neuron2, None);
-    neuron1.add_input(&mut sensor);
-    neuron1.record_input(1, 0.5);
-    scheduler.start(vec![Rc::new(neuron1), Rc::new(neuron2)]);
+    // neuron1.add_connection(&mut neuron2, None);
+    // neuron1.add_input(&mut sensor);
+    // neuron1.record_input(1, 0.5);
+    // scheduler.start(vec![Rc::new(neuron1), Rc::new(neuron2)]);
   }
 }
--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,15 +1,16 @@
+use std::cell::RefCell;
 use std::rc::Rc;
 use std::collections::HashMap;
 
 use crate::neuron::Neuron;
 
-pub struct Scheduler<'a> {
+pub struct Scheduler {
   propagating: bool,
   time: u64,
-  scheduled: HashMap<u64, Vec<Box<Neuron<'a>>>>,
+  scheduled: HashMap<u64, Vec<RefCell<Neuron>>>,
 }
 
-impl<'a> Scheduler<'a> {
+impl Scheduler {
   pub fn new() -> Self {
     Scheduler {
       propagating: false,
@@ -18,43 +19,43 @@ impl<'a> Scheduler<'a> {
     }
   }
 
-  pub fn start(&mut self, starting_objects: Vec<Box<Neuron>>) {
+
+
+  pub fn add(&mut self, time: u64, neuron: RefCell<Neuron>) {
+    if !self.scheduled.contains_key(&time) {
+      self.scheduled.insert(time, Vec::new());
+    }
+    
+    if let Some(neurons) = self.scheduled.get_mut(&time) {
+      if neurons.iter().position(|n| n.borrow().id == neuron.borrow().id).is_none() {
+        neurons.push(neuron);
+      }
+    }
+  }
+
+  pub fn start(&mut self, starting_neurons: &mut Vec<RefCell<Neuron>>) {
     self.propagating = true;
 
     while self.propagating {
       // check inputs
-      for obj in &starting_objects {
-        obj.activate(self.time);
+      for neuron in starting_neurons.iter_mut() {
+        neuron.borrow_mut().activate(self.time);
+      }
+
+      if !self.scheduled.contains_key(&self.time) {
+        println!("nothing scheduled for time {}. stop.", self.time);
+        self.propagating = false;
+        break;
       }
       self.run_scheduled(self.time);
       self.time += 1;
     }
   }
 
-  pub fn add(&mut self, time: u64, neuron: &Neuron<'a>) {
-    if !self.scheduled.contains_key(&time) {
-      self.scheduled.insert(time, Vec::new());
-    }
-    
-    if let Some(neurons) = self.scheduled.get_mut(&time) {
-      neurons.iter()
-        .position(|n| n.id == neuron.id)
-        .or_else(|| {
-          neurons.push(Box::new(*neuron));
-          None
-        });
-    }
-  }
-
   fn run_scheduled(&mut self, time: u64) {
-    if !self.scheduled.contains_key(&time) {
-      println!("nothing scheduled for time {}. stop.", time);
-      self.propagating = false;
-      return;
-    }
-
-    for neuron in self.scheduled[&time].iter() {
-      neuron.activate(time);
+    for neuron in self.scheduled[&time].iter_mut() {
+      neuron.borrow_mut()
+        .activate(time);
     }
   }
 }
@@ -62,6 +63,7 @@ impl<'a> Scheduler<'a> {
 
 #[cfg(test)]
 mod tests {
+  use std::cell::RefCell;
   use std::rc::Rc;
   use crate::neuron::Neuron;
 
@@ -69,12 +71,15 @@ mod tests {
 
   #[test]
   fn main() {
-    let mut scheduler = Rc::new(Scheduler::new());
-    let neuron = Neuron::new("test".to_string(), Rc::clone(&scheduler), 0);
-    let p_neuron = Rc::new(neuron);
-
+    let mut scheduler = RefCell::new(Scheduler::new());
+    let neuron = RefCell::new(Neuron::new(
+      "test".to_string(), 
+      scheduler, 
+      0
+    ));
+    
     // Example usage
-    scheduler.add(1, &neuron);
-    scheduler.start(vec![p_neuron]);
+    scheduler.get_mut().add(1, neuron);
+    scheduler.get_mut().start(&mut vec![neuron]);
   }
 }
